dabb5fbcad8f87e898e7b76412e626729e649b45
K-Ryo-ta <studyprograming51@gmail.com>
2023-08-12 22:38:06 +0900

    add:異なるローカルホストサーバ間でapiを叩こうとすると起こるcorsエラーを直した。

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
+const cors = require("cors")
 
 const PORT = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth",authRoute);
